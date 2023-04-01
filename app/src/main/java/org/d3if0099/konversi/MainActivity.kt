c5cc58e6f0922d128d2edec1b8096f9b518f001e
Lutfan Davi <lutfan04@gmail.com>
2023-04-01 09:31:00 +0700

    membuat perhitungan rumus konversi

--- app/src/main/java/org/d3if0099/konversi/MainActivity.kt
+++ app/src/main/java/org/d3if0099/konversi/MainActivity.kt
@@ -2,10 +2,32 @@ package org.d3if0099.konversi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if0099.konversi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConvert.setOnClickListener {
+            val celsius = binding.etCelsius.text.toString().toDoubleOrNull()
+            val fahrenheit = binding.etFahrenheit.text.toString().toDoubleOrNull()
+
+            if (celsius != null && fahrenheit == null) {
+                val result = (celsius * 9 / 5) + 32
+                binding.etFahrenheit.setText(result.toString())
+            } else if (celsius == null && fahrenheit != null) {
+                val result = (fahrenheit - 32) * 5 / 9
+                binding.etCelsius.setText(result.toString())
+            } else {
+                // Show error message
+            }
+        }
     }
 }
+
